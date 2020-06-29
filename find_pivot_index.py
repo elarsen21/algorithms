435f93d2846caa0d325ff8c590073005bde7fc66
Erik Larsen <eriklarsen@eriks-mbp-2.myfiosgateway.com>
2020-06-29 14:41:29 -0400

    Using sum()

---
+++ find_pivot_index.py
@@ -0,0 +1,16 @@
+"""
+Leetcode 724. Find Pivot Index
+
+Given an array of integers nums, write a method that returns the "pivot" index of this array.
+
+We define the pivot index as the index where the sum of all the numbers to the left of the index is equal to the sum of all the numbers to the right of the index.
+
+If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
+"""
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if sum(nums[:i]) == sum(nums[i+1:]):
+                return i
+        return -1
